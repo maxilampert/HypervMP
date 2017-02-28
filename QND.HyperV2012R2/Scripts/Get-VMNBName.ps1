a36d8e664847c87bb978979587bfaeaed398a8e5
mortenlerudjordet <git@lerun.info>
2017-02-28 14:47:01 +0100

    git ignore

--- QND.HyperV2012R2/Scripts/Get-VMNBName.ps1
+++
@@ -1,9 +0,0 @@
-﻿param($vmName)
-
-#MOM API
-$oAPI = New-Object -ComObject "MOM.ScriptAPI"
-
-$NBName = ($vmName.Split('.'))[0]
-$oBag = $oAPI.CreatePropertyBag()
-$oBag.AddValue('NetBIOSName', $NBName)
-$obag
--- QND.HyperV2012R2/Scripts/Write-TriggerEvent.ps1
+++
@@ -1,7 +0,0 @@
-﻿
-#quick and dirty
-param([string] $source)
-
-New-EventLog –LogName 'Operations Manager' –Source 'Hyper-V Dynamic Discovery' -ErrorAction SilentlyContinue
-
-Write-EventLog -LogName 'Operations Manager' -Source 'Hyper-V Dynamic Discovery' -EntryType Information -EventId 62002 -Message $source
